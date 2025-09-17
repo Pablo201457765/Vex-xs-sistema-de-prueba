1e7d4d3e149c67e1616fef9426a3f067b2dd34d0
Pablo201457765 <p9112925@gmail.com>
2025-09-17 11:48:48 -0300

    Delete main.ts

--- main.ts
+++
@@ -1,4 +0,0 @@
-/* main.ts - source for Vex XS Ultimate Plus 5.1 */
-/* This file is meant as source; main.js is the runtime used in the ZIP. */
-
-export {}; // placeholder - keep as editable source
